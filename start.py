f69e6949638af8e61d6202dd7d4138d47e0f2dca
henning998 <henninghess@hotmail.dk>
2022-12-02 12:02:24 +0100

    cd

--- start.py
+++ start.py
@@ -1,10 +1,6 @@
 import os
 
-os.system("ls")
-
-os.system("cd 2DJumperBuild")
-
-os.system("ls")
+os.system("cd 2DJumperBuild && ls")
 
 os.system("chmod +x 2DJumperBuild/2DJumperBuildLinux/2DJumperBuildLinux.x86_64")
 
