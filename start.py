1218dea9b8df0c7c4886d06e4fb63d766a800bf3
henning998 <henninghess@hotmail.dk>
2022-12-02 11:59:06 +0100

    ls

--- start.py
+++ start.py
@@ -1,5 +1,7 @@
 import os
 
+os.system("ls")
+
 os.system("chmod +x 2DJumperBuild/2DJumperBuildLinux/2DJumperBuildLinux.x86_64")
 
 os.system("python -u 2DJumperBuild/2DJumperBuildLinux/Training/Jumper2Denv.py")
