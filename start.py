4bf195b2827d8aa0085c238a53dfdb28d127c773
henning998 <henninghess@hotmail.dk>
2022-12-02 11:49:49 +0100

    .

--- start.py
+++ start.py
@@ -2,4 +2,4 @@ import os
 
 os.system("chmod +x 2DJumperBuild\2DJumperBuildLinux\2DJumperBuildLinux.x86_64")
 
-os.system("python -u docker-unity-env\2DJumperBuild\2DJumperBuildLinux\Training\Jumper2Denv.py")
+os.system("python -u 2DJumperBuild\2DJumperBuildLinux\Training\Jumper2Denv.py")
