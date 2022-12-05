4dc987410ea9ac0a245506bf176c4c8d921f7b8b
henning998 <henninghess@hotmail.dk>
2022-12-05 10:06:49 +0100

    .

--- start.py
+++ start.py
@@ -4,6 +4,6 @@ import os
 
 os.system("chmod +x 2DJumperBuild/2DJumperBuildLinux/2DJumperBuildLinux.x86_64")
 
-os.system("python -u 2DJumperBuild/2DJumperBuildLinux/Training/proto_testing.py")
+#os.system("python -u 2DJumperBuild/2DJumperBuildLinux/Training/proto_testing.py")
 
 os.system("python -u 2DJumperBuild/2DJumperBuildLinux/Training/compute_slave_client.py")
