24f86ab53800f0e726bc8486c9e45b12efbf776d
henning998 <henninghess@hotmail.dk>
2022-12-05 15:35:17 +0100

    clean up

--- start.py
+++ start.py
@@ -1,9 +1,5 @@
 import os
 
-#os.system("cd 2DJumperBuild && ls")
-
 os.system("chmod +x 2DJumperBuild/2DJumperBuildLinux/2DJumperBuildLinux.x86_64")
 
-#os.system("python -u 2DJumperBuild/2DJumperBuildLinux/Training/proto_testing.py")
-
-os.system("python -u 2DJumperBuild/2DJumperBuildLinux/Training/compute_slave_client.py")
+os.system("python -u 2DJumperBuild/Training/compute_slave_client.py docker")
