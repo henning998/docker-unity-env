80a5c4620670fc871b5f50220db0c40cbc7199f8
henning998 <henninghess@hotmail.dk>
2022-12-05 09:20:24 +0100

    update

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import os
 
-os.system("cd 2DJumperBuild && ls")
+#os.system("cd 2DJumperBuild && ls")
 
 os.system("chmod +x 2DJumperBuild/2DJumperBuildLinux/2DJumperBuildLinux.x86_64")
 
-os.system("python -u 2DJumperBuild/2DJumperBuildLinux/Training/Jumper2Denv.py")
+os.system("python -u 2DJumperBuild/2DJumperBuildLinux/Training/compute_slave_client.py")
